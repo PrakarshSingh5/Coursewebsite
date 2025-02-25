efa38dbeb016999432d26aff054c8bb7b566db7b
Prakarsh Singh <prakarshsinghlmp@gmail.com>
2025-02-25 10:14:08 +0530

    New changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import { store } from './redux/store';
 import { Link } from "react-router-dom";
 import CourseList from './components/CourseList';
 import CourseDetails from './components/CourseDetails';
@@ -9,22 +7,20 @@ import StudentDashboard from './components/StudentDashboard';
 
 const App = () => {
   return (
-    <Provider store={store}>
-      <Router>
-        <nav className="bg-gray-800 text-white p-4">
-          <div className="container mx-auto flex justify-between items-center">
-            <Link to="/" className="text-xl font-bold">Course Portal</Link>
-            <Link to="/dashboard" className="hover:text-gray-300">My Dashboard</Link>
-          </div>
-        </nav>
-        
-        <Routes>
-          <Route path="/" element={<CourseList />} />
-          <Route path="/course/:id" element={<CourseDetails />} />
-          <Route path="/dashboard" element={<StudentDashboard />} />
-        </Routes>
-      </Router>
-    </Provider>
+    <Router>
+      <nav className="bg-gray-800 text-white p-4">
+        <div className="container mx-auto flex justify-between items-center">
+          <Link to="/" className="text-xl font-bold">Course Portal</Link>
+          <Link to="/dashboard" className="hover:text-gray-300">My Dashboard</Link>
+        </div>
+      </nav>
+      
+      <Routes>
+        <Route path="/" element={<CourseList />} />
+        <Route path="/course/:id" element={<CourseDetails />} />
+        <Route path="/dashboard" element={<StudentDashboard />} />
+      </Routes>
+    </Router>
   );
 };
 
